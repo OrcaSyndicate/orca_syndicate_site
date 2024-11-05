8ad3da7474e6c879f01be2282aad4b3bdb00741e
OrcaSyndicate <andrew.m.jakubowski@gmail.com>
2024-11-05 00:32:14 -0500

    Update streamlit_app.py

--- OrcaSyndicate/streamlit_app.py
+++ OrcaSyndicate/streamlit_app.py
@@ -4,8 +4,9 @@ from st_paywall import add_auth
 
 def custom_add_auth(**kwargs):
     result = add_auth(**kwargs)
-    
+    	
 	st.markdown("""
+ 	<style>
 	div.stButton > button:first-child {
 	background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
 	}
@@ -28,6 +29,7 @@ def custom_add_auth(**kwargs):
 	color: #ffffff;
 	background-color: #0066cc;
 	}
+ 	</style>
 	“”", unsafe_allow_html=True)
     
     return result
